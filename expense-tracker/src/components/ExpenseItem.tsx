bf7a84cac08c13bcc9c881963f5908e36661aa24
Ivn-Cdrc <ivan.cedric.bulosan@gmail.com>
2023-04-29 16:30:09 +1000

    added some ExpenseItem styling

--- expense-tracker/src/components/ExpenseItem.tsx
+++ expense-tracker/src/components/ExpenseItem.tsx
@@ -1,5 +1,16 @@
+import './ExpenseItem.css'
+
+// you must only have one root element per return statement/ per jsx code snippet
 function ExpenseItem() {
-  return <h2>Expense Item!</h2>;
+  return (
+    <div className='expense-item'>
+      <div>Date</div>
+      <div className='expense-item__description'>
+        <h2>Title</h2>
+        <div className='expense-item__price'>Amount</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
