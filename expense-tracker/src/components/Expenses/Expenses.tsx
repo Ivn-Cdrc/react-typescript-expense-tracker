987b741444f6c6dd53c5b3a57b061b72b8feeb07
Ivn-Cdrc <ivan.cedric.bulosan@gmail.com>
2023-05-02 16:49:15 +1000

    Organised components into folders

--- expense-tracker/src/components/Expenses/Expenses.tsx
+++ expense-tracker/src/components/Expenses/Expenses.tsx
@@ -1,5 +1,5 @@
 import ExpenseItem from './ExpenseItem';
-import Card from './Card';
+import Card from '../UI/Card';
 import "./Expenses.css";
 
 interface Expense {
@@ -38,6 +38,11 @@ function Expenses() {
 				title={expenses[0].title}
 				amount={expenses[0].amount}
 				date={expenses[0].date}
+			/>
+      <ExpenseItem 
+				title={expenses[1].title}
+				amount={expenses[1].amount}
+				date={expenses[1].date}
 			/>
 		</Card>
 	);
